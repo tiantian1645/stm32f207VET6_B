53a6f6d8b5a298dfcddc9ae0143d28d20357f97d
miaoming <1541365185@qq.com>
2019-09-16 13:39:24 +0800

    led test task

---
+++ Inc/main.h
@@ -0,0 +1,88 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f2xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "timers.h"
+#include "queue.h"
+#include "semphr.h"
+#include "string.h"
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+void FL_Error_Handler(char * file, int line);
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LAMP1_Pin GPIO_PIN_8
+#define LAMP1_GPIO_Port GPIOA
+#define LAMP2_Pin GPIO_PIN_11
+#define LAMP2_GPIO_Port GPIOA
+#define LAMP3_Pin GPIO_PIN_12
+#define LAMP3_GPIO_Port GPIOA
+#define LED_RUN_Pin GPIO_PIN_10
+#define LED_RUN_GPIO_Port GPIOC
+#define MCU_TXD_Pin GPIO_PIN_12
+#define MCU_TXD_GPIO_Port GPIOC
+#define MCU_RXD_Pin GPIO_PIN_2
+#define MCU_RXD_GPIO_Port GPIOD
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
