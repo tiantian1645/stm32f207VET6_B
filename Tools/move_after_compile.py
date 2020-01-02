27d1f3026a5f7a1310e38fdf996efb29bc40e4a2
miaoming <1541365185@qq.com>
2020-01-02 18:27:07 +0800

    feta: keep origin output file after compile add archive

--- Tools/move_after_compile.py
+++ Tools/move_after_compile.py
@@ -1,8 +1,11 @@
 import os
+import pathlib
 import re
-from loguru import logger
 import shutil
 from datetime import datetime
+
+import stackprinter
+from loguru import logger
 
 
 def get_version_str():
@@ -20,16 +23,27 @@ def get_version_str():
 def main():
     logger.debug(f"current path | {os.path.abspath('./')}")
     version_str = get_version_str()
-    datetime_str = datetime.now().strftime(r"%Y%m%d%H%M%S")
+    now = datetime.now()
+    datetime_str = now.strftime(r"%Y%m%d%H%M%S")
+    #  复制到根目录
     target_dir = "E:\\WebServer\\DC201\\程序\\控制板\\"
     for fn in os.listdir(target_dir):
         if fn.startswith("stm32f207VET6_B-"):
             file_path = os.path.join(target_dir, fn)
             logger.debug(f"remove old file | {file_path}")
             os.remove(file_path)
-    shutil.move("../Debug/stm32f207VET6_B.bin", f"{target_dir}stm32f207VET6_B-{datetime_str}-v{version_str}.bin")
-    shutil.move("../Debug/stm32f207VET6_B.hex", f"{target_dir}stm32f207VET6_B-{datetime_str}-v{version_str}.hex")
+    shutil.copyfile("../Debug/stm32f207VET6_B.bin", f"{target_dir}stm32f207VET6_B-{datetime_str}-v{version_str}.bin")
+    shutil.copyfile("../Debug/stm32f207VET6_B.hex", f"{target_dir}stm32f207VET6_B-{datetime_str}-v{version_str}.hex")
+    #  归档
+    target_dir = f"E:\\WebServer\\DC201\\程序\\控制板\\V{version_str}\\{now.strftime('%Y%m%d_%H%M%S')}"
+    name = f"stm32f207VET6_B-{datetime_str}-v{version_str}"
+    pathlib.Path(target_dir).mkdir(parents=True, exist_ok=True)
+    shutil.copyfile("../Debug/stm32f207VET6_B.bin", os.path.join(target_dir, f"{name}.bin"))
+    shutil.copyfile("../Debug/stm32f207VET6_B.hex", os.path.join(target_dir, f"{name}.hex"))
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception:
+        logger.error(f"error in main loog \n{stackprinter.format()}")
