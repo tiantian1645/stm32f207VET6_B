25e581c8d55c92e149afcb265dad47d6b86c9e7a
miaoming <1541365185@qq.com>
2020-01-14 14:52:09 +0800

    fxi: 增加软定时器任务堆栈大小 192 -> 208

--- Inc/FreeRTOSConfig.h
+++ Inc/FreeRTOSConfig.h
@@ -78,7 +78,7 @@ extern uint32_t SystemCoreClock;
 #define configUSE_TIMERS 1
 #define configTIMER_TASK_PRIORITY (10)
 #define configTIMER_QUEUE_LENGTH 10
-#define configTIMER_TASK_STACK_DEPTH 192
+#define configTIMER_TASK_STACK_DEPTH 208
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
