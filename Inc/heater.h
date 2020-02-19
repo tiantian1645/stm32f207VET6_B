42b7b98a1f2fc725912c628bae8fea602e1e3d40
miaoming <1541365185@qq.com>
2020-02-19 10:46:15 +0800

    fix: hrater for #23

--- Inc/heater.h
+++ Inc/heater.h
@@ -17,11 +17,11 @@
 #define HEATER_BTM_DEFAULT_SETPOINT (37)
 #define HEATER_TOP_DEFAULT_SETPOINT (37)
 
-#define HEATER_BTM_OVERSHOOT_TIMEOUT (5)   /* 下加热体过冲持续时间 */
-#define HEATER_BTM_OVERSHOOT_TARGET (37.8) /* 下加热体过冲目标温度 */
+#define HEATER_BTM_OVERSHOOT_TIMEOUT (6)   /* 下加热体过冲持续时间 */
+#define HEATER_BTM_OVERSHOOT_TARGET (37.5) /* 下加热体过冲目标温度 */
 
-#define HEATER_TOP_OVERSHOOT_TIMEOUT (16)  /* 上加热体过冲持续时间 */
-#define HEATER_TOP_OVERSHOOT_TARGET (38.2) /* 上加热体过冲目标温度 */
+#define HEATER_TOP_OVERSHOOT_TIMEOUT (20)  /* 上加热体过冲持续时间 */
+#define HEATER_TOP_OVERSHOOT_TARGET (37.8) /* 上加热体过冲目标温度 */
 
 /* Exported types ------------------------------------------------------------*/
 typedef enum {
